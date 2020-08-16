7eb6ae6961fb3307ee1d8abac2e96339819cefc7
fumitoshi <fumi10_k@yahoo.co.jp>
2020-08-16 15:02:44 +0900

    a

--- geogis2/settings.py
+++ geogis2/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = '!t7o14(2!oxx(t#v*&ge4fcw0$-)bx$!heptj*fwo+$_&=5nnr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.194.42.99']
 
 
 # Application definition
